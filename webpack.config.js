1ec3e7054a7876eaf698a0c734332b9af230180f
Dima <dm_kogan@yahoo.de>
2018-10-17 23:15:03 +0200

    test bundle js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,29 @@
  * Created by Dima on 14.09.2018.
  */
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require ('path');
 
 module.exports = {
-    // ...
+    entry: ['./src/js/script1.js','./src/js/script2.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
         new BrowserSyncPlugin(
             {
